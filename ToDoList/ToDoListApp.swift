9bd55d4a3b3c443722dcd2a376b4d736312897ae
app-kaisha <app.kaihatsusha@gmail.com>
2026-01-04 21:10:50 +0000

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,18 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by app-kaihatsusha on 04/01/2026.
+//  Copyright © 2026 app-kaihatsusha. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
